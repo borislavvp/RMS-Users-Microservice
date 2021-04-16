60d94b5b876e444a873932569a180a309160dc18
Borislav Pavlov <416657@student.fontys.nl>
2021-04-16 20:00:51 +0200

    fixed seeder and added configuration for refreshing the tokens

--- Users.Data/Configuration/Config.cs
+++ Users.Data/Configuration/Config.cs
@@ -1,5 +1,6 @@
 ﻿using IdentityServer4;
 using IdentityServer4.Models;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 
 namespace Users.Data.Configuration
@@ -12,20 +13,31 @@ namespace Users.Data.Configuration
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
             };
-
         public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
+          new ApiScope[]
+          {
+          };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
             {
+                new ApiResource("orders","Orders Service"),
+                new ApiResource("products","Products Service"),
+                new ApiResource("basket","Basket Service"),
             };
 
-        public static IEnumerable<Client> Clients =>
+        public static IEnumerable<Client> Clients(IConfiguration configuration) =>
             new Client[] 
             {
                  new Client
                 {
-                    AccessTokenLifetime = 1200,
+                    AccessTokenLifetime = 50, // 2 hours
+                    //AccessTokenLifetime = 60*60*2, // 2 hours
+                    //IdentityTokenLifetime= 60*60*2 // 2 hours
                     AllowOfflineAccess = true,
                     UpdateAccessTokenClaimsOnRefresh = true,
+                    SlidingRefreshTokenLifetime = 30,
+                    RefreshTokenUsage = TokenUsage.OneTimeOnly,
                     ClientName = "TestAPI",
                     ClientId = "testApi",
                     AllowedGrantTypes = GrantTypes.Code,
@@ -34,16 +46,18 @@ namespace Users.Data.Configuration
                     RequireConsent = false,
                     RedirectUris = new List<string>()
                     {
-                        "https://localhost:4200/response"
+                        $"{configuration.GetValue<string>("CustomerWebsiteURL")}/signin-oidc",
+                        $"{configuration.GetValue<string>("CustomerWebsiteURL")}/silent-renew",
                     },
                     PostLogoutRedirectUris = new List<string>()
                     {
-                        "https://localhost:4200/response"
+                        $"{configuration.GetValue<string>("CustomerWebsiteURL")}/signout-callback-oidc"
                     },
                     AllowedScopes =
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OfflineAccess
                     }
                 }
             };
