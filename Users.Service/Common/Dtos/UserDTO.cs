15748b1c32dbb23099e1c0a932c43f044512f04c
Borislav Pavlov <416657@student.fontys.nl>
2021-04-18 11:02:45 +0200

    added user dto

---
+++ Users.Service/Common/Dtos/UserDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Users.Service.Models
+{
+    public class UserDTO
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
