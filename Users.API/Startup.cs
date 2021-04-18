43691d852fc72d9c22e89930fd291a17a4862d8e
Borislav Pavlov <416657@student.fontys.nl>
2021-04-18 14:24:56 +0200

    changed the configuration of the services to use the extension methods

--- Users.API/Startup.cs
+++ Users.API/Startup.cs
@@ -2,18 +2,12 @@
 using IdentityServer4.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Reflection;
-using Users.API.Controllers;
-using Users.Data;
-using Users.Data.Models;
+using Users.API.Extensions;
 using Users.Service;
+using Users.Service.Profile;
 
 namespace Users.API
 {
@@ -31,76 +25,19 @@ namespace Users.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddAuthentication(x => x.DefaultAuthenticateScheme = IdentityServerConstants.DefaultCookieAuthenticationScheme);
+
             services.AddDbContext(Configuration);
             services.AddControllers();
-
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationUsersDbContext>()
-                .AddDefaultTokenProviders();
-
-            services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(
-                    builder =>
-                    {
-                        builder.WithOrigins("https://localhost:4200")
-                        .AllowCredentials()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader();
-                    });
-            });
-           
-            services.AddSingleton<ICorsPolicyService>((container) => {
-                var logger = container.GetRequiredService<ILogger<DefaultCorsPolicyService>>();
-                return new DefaultCorsPolicyService(logger)
-                {
-                    AllowedOrigins = { "https://localhost:4200" }
-                };
-            });
-
-            services.ConfigureApplicationCookie(config =>
-            {
-                config.Cookie.Name = "IdentityServer.Cookie";
-                config.LoginPath = "/api/redirect/login";
-                config.LogoutPath = "/api/logout";
-            });
 
             services.AddScoped<IProfileService, ProfileService>();
             services.AddScoped<UsersService, UsersService>();
 
-            services.AddIdentityServer(options =>
+            if (Environment.IsDevelopment())
             {
-                options.IssuerUri = Configuration.GetValue<string>("IssuerUri");
-                options.Events.RaiseErrorEvents = true;
-                options.Events.RaiseInformationEvents = true;
-                options.Events.RaiseFailureEvents = true;
-                options.Events.RaiseSuccessEvents = true;
-                options.EmitStaticAudienceClaim = true;
-                options.Authentication.CookieLifetime = TimeSpan.FromHours(1);
-            })
-            .AddDeveloperSigningCredential()
-            .AddAspNetIdentity<ApplicationUser>()
-            .AddConfigurationStore(options =>
-            {
-                options.ConfigureDbContext = builder => builder.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection"),
-                    sqlServerOptionsAction: sqlOptions =>
-                    {
-                        sqlOptions.MigrationsAssembly(Assembly.GetAssembly(typeof(ApplicationUsersDbContext)).GetName().Name);
-                        sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
-                    });
-            })
-            .AddOperationalStore(options =>
-            {
-                options.ConfigureDbContext = builder => builder.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection"),
-                    sqlServerOptionsAction: sqlOptions =>
-                    {
-                        sqlOptions.MigrationsAssembly(Assembly.GetAssembly(typeof(ApplicationUsersDbContext)).GetName().Name);
-                        sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
-                    });
-            })
-            .Services.AddTransient<IProfileService, ProfileService>();
+                services.SetupDevelopmentCorsPolicies(Configuration);
+            }
+
+            services.SetupIdentityServer(Configuration);
         }
 
         public void Configure(IApplicationBuilder app)
@@ -123,22 +60,5 @@ namespace Users.API
             });
         }
     }
-    static class StartupExtensionMethods
-    {
-        public static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddDbContext<ApplicationUsersDbContext>(options =>
-                    options.UseSqlServer(
-                        configuration.GetConnectionString("DefaultConnection"),
-                        sqlServerOptionsAction: sqlOptions =>
-                        {
-                            sqlOptions.MigrationsAssembly(Assembly.GetAssembly(typeof(ApplicationUsersDbContext)).GetName().Name);
-                            sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
-                        })
-                    );
-
-            return services;
-        }
-
-    }
+    
 }
