e6ce4b013c7a778721cd9d335bd6714689c8224c
Borislav Pavlov <416657@student.fontys.nl>
2021-04-18 14:22:23 +0200

    added dtos for registering and loging

--- Users.Service/Common/Dtos/UserRegisterDTO.cs
+++ Users.Service/Common/Dtos/UserRegisterDTO.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Users.Service.Models
 {
-    public class UserDTO
+    public class UserRegisterDTO
     {
         public string Email { get; set; }
         public string FirstName { get; set; }
