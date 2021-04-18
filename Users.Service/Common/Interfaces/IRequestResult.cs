8788d0df0a7376f1d527a5fe328874335a2a2a5c
Borislav Pavlov <416657@student.fontys.nl>
2021-04-18 11:00:05 +0200

    added a class for handling the requests result

---
+++ Users.Service/Common/Interfaces/IRequestResult.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Users.Service.Common.Interfaces
+{
+    /// <summary>
+    /// Represents the object responsible for keeping the result of an authentication request
+    /// </summary>
+    public interface IRequestResult
+    {
+        /// <summary>
+        /// The reason that the action failed.
+        /// </summary>
+        string FailureReason { get; }
+
+        /// <summary>
+        /// Indication whether the action succeed.
+        /// </summary>
+        bool IsSuccess { get; }
+    } 
+    /// <summary>
+    /// Represents the object responsible for keeping the result of an authentication request with additional data property
+    /// </summary>
+    public interface IRequestResult<T>
+    {
+        /// <summary>
+        /// The data which is passed when the result is successfull
+        /// </summary>
+        T Data { get;}
+
+        /// <summary>
+        /// The reason that the action failed.
+        /// </summary>
+        string FailureReason { get; }
+
+        /// <summary>
+        /// Indication whether the action succeed.
+        /// </summary>
+        bool IsSuccess { get; }
+    }
+}
