0bad3309713d2004e60be930e2de3f3c98362198
Borislav Pavlov <416657@student.fontys.nl>
2021-05-12 16:03:15 +0300

    added env variable for the desktop app routes, added client for the restaurant gateway and a token exchange validator

--- Users.Data/Configuration/Config.cs
+++ Users.Data/Configuration/Config.cs
@@ -7,8 +7,8 @@ namespace Users.Data.Configuration
 {
     public static class Config
     {
-        private static readonly string WEBSITE_NAME = "TEST_WEBSITE";
-        private static readonly string WEBSITE_ID = "TEST_WEBSITE_ID";
+        private static readonly string DESKTOP_NAME = "DESKTOP_APP";
+        private static readonly string DESKTOP_ID = "DESKTOP_APP_ID";
 
         public static IEnumerable<IdentityResource> IdentityResources =>
             new IdentityResource[]
@@ -19,49 +19,77 @@ namespace Users.Data.Configuration
         public static IEnumerable<ApiScope> ApiScopes =>
           new ApiScope[]
           {
+                new ApiScope("orders.read"),
+                new ApiScope("orders.write"),
+                new ApiScope("meals.fullaccess"),
+                new ApiScope("basket.fullaccess"),
+                new ApiScope("proeprestaurantgateway.fullaccess"),
           };
 
         public static IEnumerable<ApiResource> ApiResources =>
             new ApiResource[]
             {
-                new ApiResource("orders","Orders Service"),
-                new ApiResource("products","Products Service"),
-                new ApiResource("basket","Basket Service"),
+                new ApiResource("proeprestaurantgateway","Restaurant App Gateway Service")
+                { 
+                    Scopes = { "proeprestaurantgateway.fullaccess" }
+                },
+                new ApiResource("orders","Orders Service")
+                { 
+                    Scopes = { "orders.read" , "orders.write" }
+                },
+                new ApiResource("meals","Meals Service")
+                {
+                    Scopes = { "meals.fullaccess" }
+                },
+                new ApiResource("basket","Basket Service")
+                {
+                    Scopes = { "basket.fullaccess" }
+                },
             };
 
         public static IEnumerable<Client> Clients(IConfiguration configuration) =>
             new Client[] 
             {
-                 new Client
+                new Client
                 {
-                    AccessTokenLifetime = 90,
-                    //AccessTokenLifetime = 60*60*2, // 2 hours
+                    ClientId = "proeprestaurantgatewaytodownstreamtokenexchangeclient",
+                    ClientName = "Gateway to Downstream Token Exchange Client",
+                    AllowedGrantTypes = new[] { "urn:ietf:params:oauth:grant-type:token-exchange" },
+                    RequireConsent = false,
+                    ClientSecrets = { new Secret("0cdea0bc-779e-4368-b46b-09956f70712c".Sha256()) },
+                    AllowedScopes = {
+                         "openid", "profile", "orders.read","meals.fullaccess" }
+                },
+                new Client
+                {
+                    AccessTokenLifetime = 60*60*2, // 2 hours
                     AllowAccessTokensViaBrowser = true,
                     UpdateAccessTokenClaimsOnRefresh = true,
-                    ClientName = WEBSITE_NAME,
-                    ClientId = WEBSITE_ID,
-                    ClientUri = $"{configuration.GetValue<string>(WEBSITE_NAME)}",
+                    ClientName = DESKTOP_NAME,
+                    ClientId = DESKTOP_ID,
+                    ClientUri = $"{configuration.GetValue<string>(DESKTOP_NAME)}",
                     AllowedGrantTypes = GrantTypes.Code,
                     RequireClientSecret = false,
                     RequirePkce = true,
                     RequireConsent = false,
                     RedirectUris = new List<string>()
                     {
-                        $"{configuration.GetValue<string>(WEBSITE_NAME)}/signin-oidc",
-                        $"{configuration.GetValue<string>(WEBSITE_NAME)}/assets/silent-callback.html",
+                        $"{configuration.GetValue<string>(DESKTOP_NAME)}/signin-oidc",
+                        $"{configuration.GetValue<string>(DESKTOP_NAME)}/assets/silent-callback.html",
                     },
                     PostLogoutRedirectUris = new List<string>()
                     {
-                        $"{configuration.GetValue<string>(WEBSITE_NAME)}/signout-callback-oidc"
+                        $"{configuration.GetValue<string>(DESKTOP_NAME)}/signout-callback-oidc"
                     },
                     AllowedScopes =
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
+                        "proeprestaurantgateway.fullaccess",
                     },
                     AllowedCorsOrigins =
                     {
-                        $"{configuration.GetValue<string>(WEBSITE_NAME)}"
+                        $"{configuration.GetValue<string>(DESKTOP_NAME)}"
                     }
                 }
             };
---
+++ Users.Service/Extensions/TokenExchangeExtensionGrantValidator.cs
@@ -0,0 +1,75 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Users.Service.Extensions
+{
+    public class TokenExchangeExtensionGrantValidator : IExtensionGrantValidator
+    {
+        private readonly ITokenValidator _validator;
+
+        public TokenExchangeExtensionGrantValidator(ITokenValidator validator)
+        {
+            _validator = validator ?? throw new ArgumentNullException(nameof(validator));
+        }
+
+        public string GrantType => "urn:ietf:params:oauth:grant-type:token-exchange";
+        private string _accessTokenType => "urn:ietf:params:oauth:token-type:access_token";
+
+        public async Task ValidateAsync(ExtensionGrantValidationContext context)
+        {
+            var requestedGrant = context.Request.Raw.Get("grant_type");
+            if (string.IsNullOrWhiteSpace(requestedGrant) || requestedGrant != GrantType)
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant,
+                    "Invalid grant.");
+                return;
+            }
+
+            var subjectToken = context.Request.Raw.Get("subject_token");
+            if (string.IsNullOrWhiteSpace(subjectToken))
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidRequest,
+                    "Subject token missing.");
+                return;
+            }
+
+            var subjectTokenType = context.Request.Raw.Get("subject_token_type");
+            if (string.IsNullOrWhiteSpace(subjectTokenType))
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidRequest,
+                    "Subject token type missing.");
+                return;
+            }
+
+            if (subjectTokenType != _accessTokenType)
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidRequest,
+                   "Subject token type invalid.");
+                return;
+            }
+
+            var result = await _validator.ValidateAccessTokenAsync(subjectToken);
+            if (result.IsError)
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant,
+                    "Subject token invalid.");
+                return;
+            }
+
+            var subjectClaim = result.Claims.FirstOrDefault(c => c.Type == "sub");
+            if (subjectClaim == null)
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidRequest,
+                    "Subject token must contain sub value.");
+                return;
+            }
+
+            context.Result = new GrantValidationResult(subjectClaim.Value, "access_token", result.Claims);
+            return;
+        }
+    }
+}
