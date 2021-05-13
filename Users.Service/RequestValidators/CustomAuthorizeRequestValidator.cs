f005aef8d9ae36767ae5c5d82acba4e7bda96267
Borislav Pavlov <416657@student.fontys.nl>
2021-05-13 17:16:17 +0300

    added customer authorization validator for granting access tokens

---
+++ Users.Service/RequestValidators/CustomAuthorizeRequestValidator.cs
@@ -0,0 +1,37 @@
+﻿using IdentityServer4.Validation;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Users.Data.Configuration;
+using Users.Data.Models;
+
+namespace Users.Service.RequestValidators
+{
+    public class CustomAuthorizeRequestValidator : ICustomAuthorizeRequestValidator
+    {
+        private IConfiguration _conf;
+        public CustomAuthorizeRequestValidator(IConfiguration conf)
+        {
+            _conf = conf;
+        }
+        public Task ValidateAsync(CustomAuthorizeRequestValidationContext context)
+        {
+            var clientID = context.Result.ValidatedRequest.ClientId;
+            var subject = context.Result.ValidatedRequest.Subject;
+
+            if(
+                clientID.Equals(AppSettingsHelper.DESKTOP_APP_ID(_conf)) && 
+                !subject.IsInRole(RoleConstants.ChorbaDeckAdmin) 
+                )
+            {
+                context.Result.IsError = true;
+                context.Result.ErrorDescription = "Access denied!";
+                context.Result.Error = "Unauthorized";
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
