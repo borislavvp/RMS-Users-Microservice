5aa7d401abc343dcd46aec3a04a3d5f12dc8fe15
Borislav Pavlov <416657@student.fontys.nl>
2021-05-13 17:14:17 +0300

    added predifined drivers

--- Users.Data/Seed/ApplicationUsersDbContextSeed.cs
+++ Users.Data/Seed/ApplicationUsersDbContextSeed.cs
@@ -20,23 +20,25 @@ namespace Users.Data.Seed
         {
             var context = scope.ServiceProvider.GetService<ApplicationUsersDbContext>();
             var userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+            var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
             context.Database.Migrate();
-            await SeedAsync(context,userManager);
+            await SeedAsync(context,userManager, roleManager);
         }
 
         private static async Task SeedAsync(
             ApplicationUsersDbContext context,
-            UserManager<ApplicationUser> userManager)
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
         {
-            if(!context.Roles.Any())
+            if (!context.Roles.Any())
             {
-                var adminRole = new IdentityRole(RoleConstants.ChorbaDeckAdmin);
-                var customerRole = new IdentityRole(RoleConstants.ChorbaDeckCustomer);
-
-                await context.Roles.AddRangeAsync(new IdentityRole[] { adminRole, customerRole });
+                await roleManager.CreateAsync(new IdentityRole(RoleConstants.ChorbaDeckAdmin));
+                await roleManager.CreateAsync(new IdentityRole(RoleConstants.ChorbaDeckDriver));
+                await roleManager.CreateAsync(new IdentityRole(RoleConstants.ChorbaDeckCustomer));
 
                 if (!context.Users.Any())
                 {
+                    /**    Admin User   **/ 
                     var adminUser = new ApplicationUser
                     {
                         Email = "chorbadeckadmin@gmail.com",
@@ -44,12 +46,61 @@ namespace Users.Data.Seed
                         FirstName = "Chorba",
                         LastName = "Deck",
                         PhoneNumber = "089891232332",
-                        Role = adminRole,
+                        Role = await roleManager.FindByNameAsync(RoleConstants.ChorbaDeckAdmin),
                         UserName = "chorbadeckadmin@gmail.com",
                         Subject = System.Guid.NewGuid().ToString()
                     };
                     var adminPassword = "@dm1nP@ssword1";
                     await userManager.CreateAsync(adminUser, adminPassword);
+                    await userManager.AddToRoleAsync(adminUser, (RoleConstants.ChorbaDeckAdmin));
+
+                    /**    Driver 1   **/
+                    var driver1 = new ApplicationUser
+                    {
+                        Email = "chorbadeckdriver1@gmail.com",
+                        Address = "Chorba Deck Driver 1 Address",
+                        FirstName = "Chorba Driver",
+                        LastName = "Deck",
+                        PhoneNumber = "089891232332",
+                        Role = await roleManager.FindByNameAsync(RoleConstants.ChorbaDeckDriver),
+                        UserName = "chorbadeckdriver1@gmail.com",
+                        Subject = System.Guid.NewGuid().ToString()
+                    };
+                    var driver1Password = "Dr1v3r1P@ssword1";
+                    await userManager.CreateAsync(driver1, driver1Password);
+                    await userManager.AddToRoleAsync(driver1, (RoleConstants.ChorbaDeckDriver));
+
+                    /**    Driver 2   **/
+                    var driver2 = new ApplicationUser
+                    {
+                        Email = "chorbadeckdriver2@gmail.com",
+                        Address = "Chorba Deck Driver2 Address",
+                        FirstName = "Chorba Driver",
+                        LastName = "Deck",
+                        PhoneNumber = "089891232332",
+                        Role = await roleManager.FindByNameAsync(RoleConstants.ChorbaDeckDriver),
+                        UserName = "chorbadeckdriver2@gmail.com",
+                        Subject = System.Guid.NewGuid().ToString()
+                    };
+                    var driver2Password = "Dr1v3r2P@ssword1";
+                    await userManager.CreateAsync(driver2, driver2Password);
+                    await userManager.AddToRoleAsync(driver2, (RoleConstants.ChorbaDeckDriver));
+
+                    /**    Driver 3   **/
+                    var driver3 = new ApplicationUser
+                    {
+                        Email = "chorbadeckdriver3@gmail.com",
+                        Address = "Chorba Deck Driver 3 Address",
+                        FirstName = "Chorba Driver",
+                        LastName = "Deck",
+                        PhoneNumber = "089891232332",
+                        Role = await roleManager.FindByNameAsync(RoleConstants.ChorbaDeckDriver),
+                        UserName = "chorbadeckdriver3@gmail.com",
+                        Subject = System.Guid.NewGuid().ToString()
+                    };
+                    var driver3Password = "Dr1v3r3P@ssword1";
+                    await userManager.CreateAsync(driver3, driver3Password);
+                    await userManager.AddToRoleAsync(driver3, (RoleConstants.ChorbaDeckDriver));
                 }
                 await context.SaveChangesAsync();
             }
